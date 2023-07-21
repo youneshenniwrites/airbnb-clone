1e4e9b19e95c33fa770ae38f798fd37f4fe247ff
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-21 13:14:59 +0100

    add menu items

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
 
 export default function UserMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -25,6 +33,7 @@ export default function UserMenu() {
           Airbnb your home
         </div>
         <div
+          onClick={toggleOpen}
           className="
           p-4
           md:py-1
@@ -47,6 +56,30 @@ export default function UserMenu() {
           </div>
         </div>
       </div>
+
+      {isOpen && (
+        <div
+          className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          "
+        >
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem label="Login" onClick={() => {}} />
+              <MenuItem label="Sign up" onClick={() => {}} />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
