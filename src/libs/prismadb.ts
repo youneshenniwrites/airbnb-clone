779902755e547f13107696bf2a33cd44522fe2e4
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-22 16:32:08 +0100

    define a prisma client instance

---
+++ src/libs/prismadb.ts
@@ -0,0 +1,12 @@
+// * This code insures the creation of one instance of prisma client during development
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
+export default client;
