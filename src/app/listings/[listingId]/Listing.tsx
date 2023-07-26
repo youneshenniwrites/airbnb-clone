154b317600a39c81fea298d2715eb492f3709857
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-26 12:00:09 +0100

    implement listing detail page heading

--- src/app/listings/[listingId]/Listing.tsx
+++ src/app/listings/[listingId]/Listing.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Container from "@/components/Container";
 import ListingHead from "@/components/listings/ListingHead";
 import { Listing, Reservation, User } from "@prisma/client";
 
@@ -9,5 +10,24 @@ type ListingProps = {
   reservation?: Reservation;
 };
 export default function Listing({ listing, currentUser }: ListingProps) {
-  return <ListingHead />;
+  return (
+    <Container>
+      <div
+        className="
+          max-w-screen-lg 
+          mx-auto
+        "
+      >
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            id={listing.id}
+            title={listing.title}
+            locationValue={listing.locationValue}
+            imageSrc={listing.imageSrc}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
+    </Container>
+  );
 }
--- src/components/listings/ListingHead.tsx
+++ src/components/listings/ListingHead.tsx
@@ -1,5 +1,61 @@
 "use client";
 
-export default function ListingHead() {
-  return <div>ListingHead</div>;
+import useCountries from "@/hooks/useCountries";
+import { User } from "@prisma/client";
+import Image from "next/image";
+import Heading from "../Heading";
+import HeartButton from "../HeartButton";
+
+type ListingHeadProps = {
+  title: string;
+  locationValue: string;
+  imageSrc: string;
+  id: string;
+  currentUser?: User | null;
+};
+
+export default function ListingHead({
+  title,
+  locationValue,
+  imageSrc,
+  id,
+  currentUser,
+}: ListingHeadProps) {
+  const { getByValue } = useCountries();
+
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+      <div
+        className="
+          w-full
+          h-[60vh]
+          overflow-hidden 
+          rounded-xl
+          relative
+        "
+      >
+        <Image
+          src={imageSrc}
+          fill
+          className="object-cover w-full"
+          alt="Image"
+        />
+        <div
+          className="
+            absolute
+            top-5
+            right-5
+          "
+        >
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
 }
