ce004a5b3d0d317afdc7337387e3f9c2db21ce69
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-27 22:24:22 +0100

    add style and state to properties page

--- src/app/properties/Properties.tsx
+++ src/app/properties/Properties.tsx
@@ -1,3 +1,73 @@
-export default function Properties() {
-  return <div>Properties</div>;
+"use client";
+
+import Container from "@/components/Container";
+import Heading from "@/components/Heading";
+import ListingCard from "@/components/listings/ListingCard";
+import { Listing, User } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import toast from "react-hot-toast";
+
+type PropertiesProps = {
+  properties: Listing[];
+  currentUser?: User | null;
+};
+export default function Properties({
+  properties,
+  currentUser,
+}: PropertiesProps) {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState("");
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+
+      axios
+        .delete(`/api/properties/${id}`)
+        .then(() => {
+          toast.success("Property deleted");
+          router.refresh();
+        })
+        .catch((error) => {
+          toast.error(error?.response?.data?.error);
+        })
+        .finally(() => {
+          setDeletingId("");
+        });
+    },
+    [router]
+  );
+
+  return (
+    <Container>
+      <Heading title="Properties" subtitle="List of your properties" />
+      <div
+        className="
+          mt-10
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {properties.map((property) => (
+          <ListingCard
+            key={property.id}
+            data={property}
+            actionId={property.id}
+            onAction={onCancel}
+            disabled={deletingId === property.id}
+            actionLabel="Delete property"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 }
--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,3 +1,27 @@
-export default function PropertiesPage() {
-  return <div>page</div>;
+import EmptyState from "@/components/EmptyState";
+import { getCurrentUser, getListings } from "@/services/backend";
+import { Listing } from "@prisma/client";
+import Properties from "./Properties";
+
+export default async function PropertiesPage() {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return <EmptyState title="Unauthorized" subtitle="Please login" />;
+  }
+
+  const properties: Listing[] = await getListings({
+    userId: currentUser?.id,
+  });
+
+  if (properties.length === 0) {
+    return (
+      <EmptyState
+        title="No properties found"
+        subtitle="Looks like you have no properties."
+      />
+    );
+  }
+
+  return <Properties properties={properties} currentUser={currentUser} />;
 }
