bf394b534577dc856cca45d7c5bbdebd8efd326a
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-29 21:12:37 +0100

    implement dynamic search navbar

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -1,10 +1,54 @@
 "use client";
 
+import useCountries from "@/hooks/useCountries";
 import useSearchModal from "@/hooks/useSearchModal";
+import { differenceInDays } from "date-fns";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 
 export default function Search() {
   const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const locationValue = params?.get("locationValue");
+  const startDate = params?.get("startDate");
+  const endDate = params?.get("endDate");
+  const guestCount = params?.get("guestCount");
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return "Anywhere";
+  }, [locationValue, getByValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return "Any Week";
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return "Add Guests";
+  }, [guestCount]);
+
   return (
     <div
       onClick={searchModal.onOpen}
@@ -35,7 +79,7 @@ export default function Search() {
             px-6
           "
         >
-          Anywhere
+          {locationLabel}
         </div>
         <div
           className="
@@ -49,7 +93,7 @@ export default function Search() {
             text-center
           "
         >
-          Any Week
+          {durationLabel}
         </div>
         <div
           className="
@@ -63,7 +107,7 @@ export default function Search() {
             gap-3
           "
         >
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div
             className="
               p-2 
